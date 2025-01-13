e3a67d4a0155a317a3543316a8054546424766cf
thamannarasik <thamannarasik@gmail.com>
2025-01-13 11:37:50 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function toggleMenu(){
-    const menu= document.querySelector(".menu-links");    
-    const icon= document.querySelector(".hamburger-icon");    
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM fully loaded and parsed!");
+    const preloader = document.getElementById("preloader");
+    if (preloader) {
+      console.log("Hiding preloader...");
+      preloader.style.display = "none";
+    } else {
+      console.error("Preloader element not found!");
+    }
+  });
